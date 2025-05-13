4c5a296b19612a1e3451132dbe48a7ce8742e085
f2firdaus <firdausnaseem@gmail.com>
2025-05-13 19:37:05 +0530

    add:home screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,10 +4,10 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+ 
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -18,12 +18,13 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+  
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="details" options={{ headerShown : false }} />
+     
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+   
+   
   );
 }
